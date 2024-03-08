e9d35dc38600be0750e65d00c08912a068e8a7c5
Fernando Flores <69826654+Fernando-FloresP@users.noreply.github.com>
2024-03-07 23:48:39 -0500

    Got changed yes
    Version downgrade for usability

--- webSocketServer/index.js
+++ webSocketServer/index.js
@@ -14,7 +14,7 @@ const wsServer = new webSocketServer({
 
 const clients = {};
 
-// This code generates unique userid for everyuser.
+// This code generates unique userid for every user.
 const getUniqueID = () => {
   const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   return s4() + s4() + '-' + s4();
